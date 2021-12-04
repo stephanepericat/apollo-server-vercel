60a64ab9627534af048d9e5a99efedcc210795a1
Stephane P. Pericat <contact@pericat.net>
2021-12-04 16:02:54 -0600

    apollo

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-// import { ApolloServer } from 'apollo-server-express';
-// import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 // import http from "http";
 import express from "express";
 import cors from "cors";
@@ -7,11 +7,34 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
 app.use(cors());
 app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("*", (req, res) => {
-  res.json({ ok: true, method: req.method });
-});
+const startApolloServer = async(app) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  app.use(server.getMiddleware());
+}
+
+startApolloServer(app);
 
 export default app;
