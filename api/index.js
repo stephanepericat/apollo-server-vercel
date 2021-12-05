413605e3fedfb2bef71bc538bbc449df75666c01
Stephane P. Pericat <contact@pericat.net>
2021-12-05 12:08:12 -0600

    code cleanup

--- api/index.js
+++ api/index.js
@@ -5,6 +5,11 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const httpServer = http.createServer(app);
 
 const typeDefs = gql`
@@ -18,10 +23,6 @@ const resolvers = {
     hello: () => 'world',
   },
 };
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 const startApolloServer = async(app, httpServer) => {
   const server = new ApolloServer({
